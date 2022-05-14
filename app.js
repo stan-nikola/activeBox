5c2fdb168837509b7cfc2f39061cba96dc7eb312
StanislavKarmanov <stanislav.karmanov1@gmail.com>
2022-05-14 12:42:39 +0300

    add SmoothScroll

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 $(function () {
 
-    // FixedHeader 
+    
+    /*FixedHeader */
 
     let header = $("#header");
     let intro = $("#intro");
-    let introH;
+    let introH=intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    checkScroll(scrollPos, introH);
 
-    $(window).on("scroll load resize", function () {
+    $(window).on("scroll resize", function () {
         
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH);
 
-        if (scrollPos > introH)
-        { header.addClass("fixed");}
-        else
-        {header.removeClass("fixed"); }
-            
-            
-        
-        
-        // console.log(scrollPos);
+       
+                   
 
     });
 
-    
+    function checkScroll(scrollPos, introH) {
+        
+         if (scrollPos > introH)
+        { header.addClass("fixed");}
+        else
+        { header.removeClass("fixed"); }
+    }
+
+    /*SmoothScroll */
+    $("[data-scroll]").on("click", function(event) {
+
+        event.preventDefault();
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop:elementOffset -70
+
+        }, 700);
+        
+    });
+
 
     
 
-}
-);
+});
     
 
 
